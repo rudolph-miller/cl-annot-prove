91951f732b51c38f8cea09f9b4344400495a0575
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2015-06-29 02:59:28 +0900

    forgot to add new files.

---
+++ t/struct.lisp
@@ -0,0 +1,10 @@
+(in-package :cl-user)
+(defpackage cl-annot-prove-test.struct
+  (:use :cl
+        :cl-annot-prove
+        :prove))
+(in-package :cl-annot-prove-test.struct)
+
+(plan nil)
+
+(finalize)
