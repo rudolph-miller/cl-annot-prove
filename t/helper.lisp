c94af31574c9a783e5a37e8eccfcb74549211290
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2015-07-01 01:34:56 +0900

    add tests of helper.

--- t/helper.lisp
+++ t/helper.lisp
@@ -7,13 +7,55 @@
 
 (plan nil)
 
+
+(tests ((is (add 1 2) 3)
+        (is (add 2 3) 5))
+       (defun add (a b)
+         (+ a b)))
+
 (subtest "query-symbol-tests"
-  (skip 1 "Not written yet."))
+  (macrolet ((found (args comment)
+               `(ok (>= (length (query-symbol-tests ,@args)) 1)
+                    ,comment))
+
+             (not-found (args comment)
+               `(is (query-symbol-tests ,@args)
+                    nil
+                    ,comment)))
+    (subtest "found"
+      (found (:symbol 'add)
+             "with :symbol.")
+
+      (found (:symbol-name "ADD")
+             "with :symbol-name.")
+
+      (found (:symbol-package "CL-ANNOT-PROVE-TEST.HELPER")
+             "with :symbol-package string.")
+
+      (found (:symbol-package *package*)
+             "with :symbol-package package."))
+
+    (subtest "not found"
+      (not-found (:symbol 'not-found)
+                 "with :symbol.")
+
+      (not-found (:symbol-name "NOT-FOUND")
+                 "with :symbol-name."))
+
+      (not-found (:symbol-package "CL-ANNOT-PROVE")
+             "with :symbol-package string.")
+
+      (not-found (:symbol-package (find-package :cl-annot-prove))
+             "with :symbol-package package.")))
 
 (subtest "run-symbol-tests"
-  (skip 1 "Not written yet."))
+  (ok (with-output-to-string (*standard-output*)
+        (run-symbol-tests (car (query-symbol-tests :symbol 'add))))
+      "can run symbol-tests-tests."))
 
 (subtest "run-package-tests"
-  (skip 1 "Not written yet."))
+  (ok (with-output-to-string (*standard-output*)
+        (run-package-tests *package*))
+      "can run package tests."))
 
 (finalize)
